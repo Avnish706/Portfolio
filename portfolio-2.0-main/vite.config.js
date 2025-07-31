b70d5719a2f332c7538bd77601bd1c2eb2684414
Avnish <Avnish1111jha@gmail.com>
2025-07-31 13:30:53 +0530

    Configure vite for deploy

--- portfolio-2.0-main/vite.config.js
+++ portfolio-2.0-main/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // important for deployment (especially on custom domains)
+  build: {
+    outDir: 'dist',
+  },
 })
