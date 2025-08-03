0d24a4a104b8d67ab6ceb8b7420951741b58694b
Avnish <Avnish1111jha@gmail.com>
2025-08-03 13:15:41 +0530

    add resume

--- portfolio-2.0-main/src/components/home/Home.jsx
+++ portfolio-2.0-main/src/components/home/Home.jsx
@@ -4,7 +4,7 @@ import "./home.css";
 import profileImg from "../../assets/photo.jpg";
 import shapeOne from "../../assets/shape-1.png";
 import shapeTwo from "../../assets/shape-2.png";
-import CV from "../../assets/updated_Avnish_Resume.pdf";
+import CV from "../../assets/Updated_Avnish_Resume.pdf";
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 const Home = () => {
